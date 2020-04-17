e131ff19f9eb8356b20610992f78ed81676aca53
David Corredor <vidmore8@gmail.com>
2020-04-16 21:16:56 -0500

    fixes

--- shell.h
+++ shell.h
@@ -51,22 +51,18 @@ char *_strncat(char *dest, char *src, int n);
 int _strlen(char *s);
 char *str_concat(char *s1, char *s2);
 char *_strchr(char *s, char c);
-char *_strchr(char *s, char c);
 int _strcmp(char *s1, char *s2);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 size_t _strcspn(const char *s1, const char *s2);
 char *_strdup(char *str);
-char *e_strdup(char *os, char *str);
 int _strncmp(const char *first, const char *last, size_t count);
-int _atoi(char *s);
-int _isdigit(int c);
+
 /* End */
 
 /* Structs functions */
 
 path_t *add_node(path_t **head, char *str);
-size_t print_list(const path_t *h);
 void free_list(path_t *head);
 
 /* End */
@@ -90,15 +86,12 @@ void print_exit_code(char *exec, int num, char *error);
 
 /* Built in functions */
 
-int exito(char *buffer, char *exec, int err_counter);
 
 /* End */
 
 /* Buffer handler functions */
 
-char *disarm_buffer(char *buffer);
 int count_buffer(char *buffer);
-char *make_string(char **disarmed);
 char **create_exec_buffer(char *buffer);
 int validate_buffer(char *buffer, int err_counter);
 int exec_command(char **buffer, char *exec, int err_counter);
@@ -108,22 +101,13 @@ int exec_command(char **buffer, char *exec, int err_counter);
 /* Shell & Path functions */
 
 path_t *get_path(void);
-size_t check_path(const path_t *head, char *str2);
-size_t exec_path(const path_t *head, char *str2);
 int lexer(char *values[]);
-void get_dir(void);
 char *_getenv(char *envp);
 int validate_command(char **buffer, char *exec, int err_counter);
-int one_param(char *buffer);
-int is_flag(char *string);
 size_t _getline(char **lineptr, size_t *n, FILE *stream);
-char *_fgets(char *s, int size, FILE *stream);
-int _getc(FILE *stream);
-int _fgetc(FILE *stream);
 char *get_route(char *command);
 char *_strtok(char *str, char *delim);
 int env(void);
-char *_findenv(const char *name, int *offset);
 
 /* End */
 
